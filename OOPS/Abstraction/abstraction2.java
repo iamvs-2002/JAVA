a96152e164999b0631c28af935e1dcead13ebd07
Vaibhav Singhal <iamvs2002@gmail.com>
2020-11-29 16:02:11 +0530

    Update abstraction2.java

--- OOPS/Abstraction/abstraction2.java
+++ OOPS/Abstraction/abstraction2.java
@@ -1,1 +1,21 @@
+//Abstraction using Interfaces
 
+interface CarStart{    
+    void start();    
+}    
+interface CarStop{    
+    void stop();    
+}    
+public class abstraction2 implements CarStart, CarStop{    
+    public void start()  {  
+        System.out.println("start");  
+    }    
+    public void stop(){  
+        System.out.println("stop");  
+    }    
+    public static void main(String args[]){    
+        abstraction2 c = new abstraction2();    
+        c.start();    
+        c.stop();    
+    }  
+}  
