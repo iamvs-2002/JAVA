b062f01d9941a325550dd411d4141e8bc93692be
Vaibhav Singhal <iamvs2002@gmail.com>
2020-11-29 16:05:27 +0530

    Update polymorphism3.java

--- OOPS/Polymorphism/polymorphism3.java
+++ OOPS/Polymorphism/polymorphism3.java
@@ -1,1 +1,17 @@
+class Bike{  
+    int speedlimit=90;  
+}  
+class Honda3 extends Bike{  
+    int speedlimit=150;  
+}
+public class polymorphism3{
+    public static void main(String args[]){     
+        Bike obj=new Honda3();  
+        Honda3 x=new Honda3();  
+        System.out.println(obj.speedlimit);//90  
+        System.out.println(x.speedlimit);//150  
+    }  
+}
+
+/*Runtime polymorphism can't be achieved by data members.*/
 
