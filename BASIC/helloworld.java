e664ebee3c96351d55feed8366d8abacd1afb651
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-18 00:05:23 +0530

    Update helloworld.java

--- BASIC/helloworld.java
+++ BASIC/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld
 public : means that anyone can run this program
 class : all codes in Java must belong to a class
 helloworld : the name of our class
-
+The name of the class must be the final name if it is declared public, else the class name can be anything.
 */
 
 {
