e7c015fb704afb2205bb3a25109fb2fa19ad35bc
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-18 00:05:12 +0530

    Update helloworld.java

--- BASIC/helloworld.java
+++ BASIC/helloworld.java
@@ -1,1 +1,12 @@
 //Hello World program in JAVA
+public class helloworld
+/*
+public : means that anyone can run this program
+class : all codes in Java must belong to a class
+helloworld : the name of our class
+
+*/
+
+{
+
+}
