0843c172368915072de7fad3bd610f71d3c0fb05
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-18 00:13:05 +0530

    Update helloworld.java

--- BASIC/helloworld.java
+++ BASIC/helloworld.java
@@ -21,7 +21,7 @@ The name of the class must be the final name if it is declared public, else the 
     System.out.println("Hello World!");
     /*
     System.out.println : the name of the method we want to call, in this case the method to print something on the screen
-    
+    ("Hello World!") : the parameter passed to this method, in this case the string/message we want to print
     */
     
   }
