13251de7cb93b739a6e38fc54acc162e19b40599
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-19 12:49:23 +0530

    Update constructors.java

--- BASIC/constructors.java
+++ BASIC/constructors.java
@@ -6,7 +6,13 @@ public class Puppy {
       System.out.println("Constructor with 0 arguments");  
    }
    public static void main(String []args) { // Following statement would create an object myPuppy
-      Puppy myPuppy = new Puppy( "tommy" ); //Passed Name is :tommy
-      Puppy myPuppy = new Puppy(); //Constructor with 0 arguments
+      Puppy myPuppy = new Puppy( "tommy" );
+      Puppy myPuppy = new Puppy(); 
    }
 }
+
+/*
+Output:
+Passed Name is :tommy
+Constructor with 0 arguments
+*/
