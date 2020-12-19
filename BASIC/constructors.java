b77b4d087cae6cc614661169d423b7fa9bc8a974
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-19 12:48:51 +0530

    Update constructors.java

--- BASIC/constructors.java
+++ BASIC/constructors.java
@@ -1,1 +1,12 @@
-
+public class Puppy {
+   public Puppy(String name) {// This constructor has one parameter, name.
+      System.out.println("Passed Name is :" + name );
+   }
+   public Puppy(){
+      System.out.println("Constructor with 0 arguments");  
+   }
+   public static void main(String []args) { // Following statement would create an object myPuppy
+      Puppy myPuppy = new Puppy( "tommy" ); //Passed Name is :tommy
+      Puppy myPuppy = new Puppy(); //Constructor with 0 arguments
+   }
+}
