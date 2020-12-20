1b7a02313846694fa1fe884205660f9c48eda36c
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-20 20:52:23 +0530

    Update and rename armstrongnumber.java to armstrongnumbers.java

---
+++ Various Java Programs/BASIC/armstrongnumbers.java
@@ -0,0 +1,23 @@
+// 153 = 1^3 + 5^3 + 3^3
+class ArmstrongNumber
+{
+	public static void main(String[] arg)
+	{
+	int i=1,a,arm,n,temp;
+	System.out.println("Armstrong numbers between 1 to 500 are");
+	while(i<500)
+	{
+	n=i;
+	arm=0;
+	while(n>0)
+	{
+		a=n%10;
+		arm=arm+(a*a*a);
+		n=n/10;
+	}
+	if(arm==i)
+		System.out.println(i);
+	i++;
+	}
+	}
+}
