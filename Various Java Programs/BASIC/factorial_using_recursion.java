a97d8e978009f9b2c8ab8fc0ae68bc86b3369cbd
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-20 20:36:22 +0530

    Update factorial_using_recursion.java

--- Various Java Programs/BASIC/factorial_using_recursion.java
+++ Various Java Programs/BASIC/factorial_using_recursion.java
@@ -1,1 +1,26 @@
-
+class FactorialUsingRecursion
+{ 
+	public static void main(String arg[])	
+	{
+	   long n;
+             	
+           Scanner sc=new Scanner(System.in);
+ 
+	   System.out.println("enter number");
+	   
+           n=sc.nextLong();
+ 
+	   long f=FactorialUsingRecursion.fact(n);
+ 
+	   System.out.println("factorial="+f);
+	}
+ 
+	static long fact(long n)
+	 {
+	     if(n<=0)
+	      return 1;
+	      
+              return FactorialUsingRecursion.fact(n-1)*n; 
+	}
+  	        	 
+}
