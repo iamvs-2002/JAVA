c2948ca5f794dd0708f4d8abd44caf5b04027736
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-30 19:09:14 +0530

    Update fibonacci.java

--- Various Java Programs/BASIC/fibonacci.java
+++ Various Java Programs/BASIC/fibonacci.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+public class fibonacci { 
+  
+    private static Scanner sc = new Scanner(System.in);
+  
+    public static void main(String args[]) 
+    { 
+        System.out.println("Kindly enter the number of fibonacci numbers required: ")
+        int N = sc.nextInt(); 
+      
+        int a=0, b=1; 
+
+          for(int i=0;i<N;i++){
+            
+              System.out.print(a + " "); 
+
+              int c = b + a; 
+              a = b; 
+              b = c; 
+          } 
+    } 
+} 
